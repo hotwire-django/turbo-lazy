65847e8f96be2726b70fc42a6a1fdf28c0225e78
Julian Feinauer <j.feinauer@pragmaticminds.de>
2020-12-27 21:05:06 +0100

    Added "real" test

---
+++ tests/test_lazy.py
@@ -0,0 +1,11 @@
+import pytest
+
+from turbo.lazy.templatetags.lazy import is_int
+
+
+def test_is_int_with_int():
+    assert is_int(123)
+
+
+def test_is_int_with_string():
+    assert not is_int("123")
